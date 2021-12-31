ba808bdf674231e80118c2dd03da807574c46143
smolgumball <noodlesips@protonmail.com>
2021-12-31 02:01:02 -0700

    update rmdeep

--- src/bitburner-vue/bin/rmDeep.js
+++ src/bitburner-vue/bin/rmDeep.js
@@ -13,6 +13,8 @@ export async function main(ns) {
   let files = ns.ls(options.host)
   let filesToRemove = []
 
+  monkeyPatchModalCss(true)
+
   if (startsWith && !endsWith) {
     filesToRemove.push(...files.filter((f) => f.startsWith(startsWith)))
   }
@@ -34,8 +36,7 @@ export async function main(ns) {
 
   if (!filesToRemove.length) {
     ns.tprint(
-      `No files found matching filters: --include-start=${startsWith}, ` +
-        `--include-end=${endsWith}, --host=${options.host}`
+      `No files found matching filters: starts-with=${startsWith}, ` + `--ends-with=${endsWith}, --host=${options.host}`
     )
   }
 
@@ -63,8 +64,47 @@ This cannot be undone unless you have previously made backups.
   } else {
     ns.tprint(`Aborted deletion of ${filesToRemove.length} files from '${options.host}'`)
   }
+
+  monkeyPatchModalCss(false)
 }
 
 function toJson(value) {
   return JSON.stringify(value, null, ' ')
+}
+
+function monkeyPatchModalCss(enableDisable = true) {
+  const doocument = globalThis['document']
+  let existingModalPatchMaybe = doocument.getElementById('__sglModalPatch')
+
+  if (enableDisable && !existingModalPatchMaybe) {
+    doocument.querySelector('body').insertAdjacentHTML(
+      'afterbegin',
+      `
+      <style id="__sglModalPatch" type="text/css">
+        /* Modal root fixes */
+        .MuiModal-root .MuiBox-root {
+          white-space: pre;
+          margin-bottom: 0;
+        }
+
+        /* Copy container fixes */
+        .MuiModal-root .MuiBox-root > .MuiTypography-root {
+          padding-bottom: 30px;
+        }
+
+        /* Button fixes */
+        .MuiModal-root .MuiBox-root > .MuiTypography-root + div {
+          position: sticky;
+          bottom: 0;
+          background-color: rgba(0,0,0,0.5);
+          margin-left: -16px;
+          margin-right: -16px;
+          padding: 1em;
+        }
+      </style>
+    `
+    )
+  } else {
+    existingModalPatchMaybe.remove()
+  }
 }
